6ffa5ad207a6d9557ce1dfd4cb77a5a6506eb925
Viliam Dias <viliamjr@gmail.com>
2014-04-03 20:14:56 -0300

    Login page (session only) working with martini render.

--- microblog.go
+++ microblog.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/sessions"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Olá Mundo")
+
+	session_store := sessions.NewCookieStore([]byte("A-Tonga-da-Mironga-do-Kabulete"))
+
+	m := martini.Classic()
+
+	fmt.Println("Environment: " + martini.Env)
+
+	m.Use(sessions.Sessions("user_control", session_store))
+	m.Use(render.Renderer())
+
+	m.Get("/", Index)
+	m.Post("/login", Login)
+	m.Get("/logout", Logout)
+	m.Run()
+}
+
+func Index(session sessions.Session, r render.Render) {
+
+	var data = struct{ Username string }{}
+	v := session.Get("username")
+	if v != nil {
+		data.Username = v.(string)
+	}
+	r.HTML(200, "index", data)
+}
+
+func Login(session sessions.Session, r *http.Request, w http.ResponseWriter) {
+
+	session.Set("username", r.FormValue("username"))
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func Logout(session sessions.Session, r *http.Request, w http.ResponseWriter) {
+
+	session.Delete("username")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
